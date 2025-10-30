7edc775483f1e05155b5ef5bb46dcea3f006008b
akankshabharti19 <akankshabh325@gmail.com>
2025-10-30 18:25:37 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173
+    host: '0.0.0.0',
+    port: 5173,
   },
-  base: './'
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  base: './',
 })
+
